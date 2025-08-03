052b521d835db462591345907061c61a8e17ed1a
Gwantana Kiboigo <gwantanak.3@gmail.com>
2025-08-03 12:22:36 -0600

    V3.6.4 | The shared file was highkey not nessaccary but well keep it for now ig

--- front-end/JS/shared.js
+++ front-end/JS/shared.js
@@ -1,15 +1,3 @@
 export function wait(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-export let driverFormHTML = "";
-export let circuitSectionHTML = "";
-export let circuitSectionVisable;
-
-export function setcircuitSectionHTML(html) {
-    circuitSectionHTML = html;
-}
-
-export function setCircuitSectionHTML(html) {
-  circuitSectionHTML = html;
-}
